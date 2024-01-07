759dd8abdcf769d2d5ab3102bebf1e123650868d
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-07 04:19:54 +0100

    hotfix bracket

--- burger-js/app.js
+++ burger-js/app.js
@@ -1,34 +1,34 @@
 
-// const burger = document.querySelector(".main_header__burger")
-// const sidebar = document.querySelector(".burger_sidebar")
+const burger = document.querySelector(".main_header__burger")
+const sidebar = document.querySelector(".burger_sidebar")
 
-// // const showElement = (elem) => elem.classList.add("show")
-// const showElement = () => sidebar.classList.toggle("show")
+// const showElement = (elem) => elem.classList.add("show")
+const showElement = () => sidebar.classList.toggle("show")
 
-// burger.addEventListener("click", showElement)
-// console.log(sidebar.classList)
+burger.addEventListener("click", showElement)
+console.log(sidebar.classList)
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtén elementos del DOM
-    var burgerNav = document.querySelector('.main_header__burger');
-    var sidebar = document.querySelector('.burger_sidebar');
-    var closeBtn = document.querySelector('.burger_sidebar__close');
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Obtén elementos del DOM
+//     var burgerNav = document.querySelector('.main_header__burger');
+//     var sidebar = document.querySelector('.burger_sidebar');
+//     var closeBtn = document.querySelector('.burger_sidebar__close');
 
-    // Agrega evento de clic a la hamburguesa
-    burgerNav.addEventListener('click', function() {
-        // Alternar la clase show en la barra lateral
-        //sidebar.classList.toggle('show');
+//     // Agrega evento de clic a la hamburguesa
+//     burgerNav.addEventListener('click', function() {
+//         // Alternar la clase show en la barra lateral
+//         //sidebar.classList.toggle('show');
 
-        // Agrega o quita la clase hide en el contenido principal
-        document.querySelector('.burger_sidebar').classList.toggle('hide');
-    });
+//         // Agrega o quita la clase hide en el contenido principal
+//         document.querySelector('.burger_sidebar').classList.toggle('show');
+//     });
 
-    // Agrega evento de clic al botón de cerrar la barra lateral
-    closeBtn.addEventListener('click', function() {
-        // Quita la clase show de la barra lateral
-        sidebar.classList.remove('show');
+//     // Agrega evento de clic al botón de cerrar la barra lateral
+//     closeBtn.addEventListener('click', function() {
+//         // Quita la clase show de la barra lateral
+//         sidebar.classList.remove('show');
 
-        // Agrega la clase hide al contenido principal
-        document.querySelector('.landing').classList.add('hide');
-    });
-});
+//         // Agrega la clase hide al contenido principal
+//         document.querySelector('.landing').classList.add('hide');
+//     });
+// });
