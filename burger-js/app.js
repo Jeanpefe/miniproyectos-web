3e0cc5a8a08b331c3fc11b2cd8eca56dbb4561f1
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-08 20:02:45 +0100

    close when close button is clicked

--- burger-js/app.js
+++ burger-js/app.js
@@ -1,21 +1,21 @@
 
-const burger = document.querySelector(".main_header__burger")
-const sidebar = document.querySelector(".burger_sidebar")
-const closeSidebar = document.querySelector(".burger_sidebar__close")
-
-// const showElement = (elem) => elem.classList.add("show")
-const showElement = () => sidebar.classList.toggle("show")
-
-burger.addEventListener("click", showElement)
-console.log(sidebar.classList)
-
-
-
 // const burger = document.querySelector(".main_header__burger")
 // const sidebar = document.querySelector(".burger_sidebar")
 // const closeSidebar = document.querySelector(".burger_sidebar__close")
 
-// const showSidebar = () => sidebar.classList.toggle("show")
-// const closeSidebar = () => sidebar.classList.toggle("hide")
-// burger.addEventListener("click", showSidebar)
-// closeSidebar.addEventListener("click", closeSidebar)
+// // const showElement = (elem) => elem.classList.add("show")
+// const showElement = () => sidebar.classList.toggle("show")
+
+// burger.addEventListener("click", showElement)
+// console.log(sidebar.classList)
+
+
+
+const burger = document.querySelector(".main_header__burger")
+const sidebar = document.querySelector(".burger_sidebar")
+const closeSidebar = document.querySelector(".burger_sidebar__close")
+
+const showSidebar = () => sidebar.classList.toggle("show")
+
+burger.addEventListener("click", showSidebar)
+closeSidebar.addEventListener("click", showSidebar)
