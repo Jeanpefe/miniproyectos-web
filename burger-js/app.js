ef98054b5ce99ad74d2c213fb6f7674777731e1a
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-08 20:00:22 +0100

    show sidebar when click on burger

--- burger-js/app.js
+++ burger-js/app.js
@@ -1,6 +1,7 @@
 
 const burger = document.querySelector(".main_header__burger")
 const sidebar = document.querySelector(".burger_sidebar")
+const closeSidebar = document.querySelector(".burger_sidebar__close")
 
 // const showElement = (elem) => elem.classList.add("show")
 const showElement = () => sidebar.classList.toggle("show")
@@ -8,27 +9,13 @@ const showElement = () => sidebar.classList.toggle("show")
 burger.addEventListener("click", showElement)
 console.log(sidebar.classList)
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Obtén elementos del DOM
-//     var burgerNav = document.querySelector('.main_header__burger');
-//     var sidebar = document.querySelector('.burger_sidebar');
-//     var closeBtn = document.querySelector('.burger_sidebar__close');
 
-//     // Agrega evento de clic a la hamburguesa
-//     burgerNav.addEventListener('click', function() {
-//         // Alternar la clase show en la barra lateral
-//         //sidebar.classList.toggle('show');
 
-//         // Agrega o quita la clase hide en el contenido principal
-//         document.querySelector('.burger_sidebar').classList.toggle('show');
-//     });
+// const burger = document.querySelector(".main_header__burger")
+// const sidebar = document.querySelector(".burger_sidebar")
+// const closeSidebar = document.querySelector(".burger_sidebar__close")
 
-//     // Agrega evento de clic al botón de cerrar la barra lateral
-//     closeBtn.addEventListener('click', function() {
-//         // Quita la clase show de la barra lateral
-//         sidebar.classList.remove('show');
-
-//         // Agrega la clase hide al contenido principal
-//         document.querySelector('.landing').classList.add('hide');
-//     });
-// });
+// const showSidebar = () => sidebar.classList.toggle("show")
+// const closeSidebar = () => sidebar.classList.toggle("hide")
+// burger.addEventListener("click", showSidebar)
+// closeSidebar.addEventListener("click", closeSidebar)
